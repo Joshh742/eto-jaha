057d3d21fd179b4f66465854646e2146057b1d22
Busdan_Joshua <141643402+Joshh742@users.noreply.github.com>
2024-10-24 08:28:02 +0700

    Add files via upload

---
+++ front-end-d/pertemuan-20/index.js
@@ -0,0 +1,4 @@
+//Introducton to Github
+// git pull orign master - untuk mengambil atau menarik code terbaru yang di update.
+console.log("Hello World");
+// front end - d
---
+++ front-end-d/pertemuan-5/exercise2.js
@@ -0,0 +1,4 @@
+//Ubah exercise 1 ttg BMI
+//dengan menggunakan:
+//1. IIFE
+//2. Callback
