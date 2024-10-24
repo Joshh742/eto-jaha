a1eb43602eda68df9b5d750d7225c21660858655
Busdan_Joshua <141643402+Joshh742@users.noreply.github.com>
2024-10-24 08:54:16 +0700

    Delete front-end-d directory

--- front-end-d/pertemuan-20/index.js
+++
@@ -1,4 +0,0 @@
-//Introducton to Github
-// git pull orign master - untuk mengambil atau menarik code terbaru yang di update.
-console.log("Hello World");
-// front end - d
--- front-end-d/pertemuan-5/exercise2.js
+++
@@ -1,4 +0,0 @@
-//Ubah exercise 1 ttg BMI
-//dengan menggunakan:
-//1. IIFE
-//2. Callback
